53ae050481e04da318cb46a446d72ab2e1cb051c
Jonatas Craveiro <jonatascraveiro@gmail.com>
2025-06-04 18:47:28 -0400

    feat(pwa): instalado recurso pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react(), nodePolyfills(), VitePWA({ registerType: 'autoUpdate' })],
   optimizeDeps: {
     include: ['@ckeditor/ckeditor5-build-classic'],
   },
