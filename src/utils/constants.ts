13d5db0557ed278b773bee906eb86be6c0b5a335
Jonatas Craveiro <jonatascraveiro@gmail.com>
2025-07-22 02:32:25 -0400

    feat: adicionar suporte ao DevTools do Apollo Client

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
 export const apiUrl = import.meta.env.VITE_API_URL as string
 export const APP_NAME = import.meta.env.VITE_APP_NAME as string
+export const IS_DEV = import.meta.env.VITE_ENV === 'development'
